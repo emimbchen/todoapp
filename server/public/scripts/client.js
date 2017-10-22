339b9b50ed684b82335ff0710ca71136073ec7f8
Emi Chen <emimbchen@gmail.com>
2017-10-22 00:27:21 -0500

    started check list function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,18 +1,37 @@
 console.log('js in');
 $(document).ready(goQuery);
 
-function goQuery(){
+function goQuery() {
     console.log('jq in');
     $('#createButton').on('click', createList);
     getTasks();
     $('#listContainer').on('click', '.createButton', addTask);
     $('#listContainer').on('click', '.deleteButton', deleteButton);
     $('#listContainer').on('click', '.hideButton', hidButton)
+    $("#listContainer").on('click','.form-check-label', doneUpdate);
+
+}
+
+//update checked boxes events
+function doneUpdate() {
+    var checkedId = $(this).attr('id');
+    if($('.form-check-label').checked){
+        console.log('this is a checked thing');
+    // $.ajax({
+    //     method: 'UPDATE',
+    //     url: '/task/' + checkedId,
+    //     data: {
+    //         complete: true
+    //     }
+    // }).done(function(response){
+    //     console.log(response);
+    // })
+    }
 }
 
 //function to create a task, send to database, append to dom
-function addTask(){
-    
+function addTask() {
+
     var task = $(this).prev().val();
     $(this).prev().val("");
     console.log(task);
@@ -28,7 +47,7 @@ function addTask(){
 }
 
 //function to create list when #createButton is pressed
-function createList(){
+function createList() {
     //get value from input
     var listName = $("#listname").val();
     $('#listname').val('');
@@ -40,43 +59,43 @@ function createList(){
 }
 
 //function to post data to server
-function postData(objectIn){
-    $.ajax ({
+function postData(objectIn) {
+    $.ajax({
         method: 'POST',
         url: '/task',
         data: { objectIn }
-    }).done(function(response){
-       console.log(response);
-       getTasks();
+    }).done(function (response) {
+        console.log(response);
+        getTasks();
     });
 }
 
 //function to get data from server
-function getTasks(){
+function getTasks() {
     $.ajax({
         method: 'GET',
         url: '/task',
-    }).done(function(response){
+    }).done(function (response) {
         //append list names and tasks
         appendAll(response);
     });
 }
 
 //delete entire list
-function deleteButton(evt){
+function deleteButton(evt) {
     evt.preventDefault();
     var id = $(this).parent().parent().attr('id');
-    $.ajax ({
+    $.ajax({
         method: 'DELETE',
-        url: '/task/'+ id
-    }).done(function(response){
+        url: '/task/' + id
+    }).done(function (response) {
         console.log(response);
         getTasks();
     })
 }
 
 //hide list
-function hidButton(evt){
+function hidButton(evt) {
     evt.preventDefault();
     console.log('hide button pressed');
 }
@@ -88,16 +107,19 @@ var createButton = '<button class="btn btn-success createButton">Create Task</bu
 // for add button after taskInput '<button class= "addButton btn btn-primary" data-list =' + array[i].list + '>Create</button></div>';
 //takes in array appends all to DOM
 
-function appendAll(array){
+function appendAll(array) {
     $('#listContainer').empty();
-    for (var i = 0; i < array.length; i++){
-        if (array[i].title == true){
-            $('#listContainer').append('<div id='+ array[i].id + '><form class="form-inline><div class="form-group"><h3>'+ array[i].list +'</h3>' + hideButton + delButton + '</div> <div class="form-inline"><input type="text">' + createButton+ '</div></form></div>');
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].title == true) {
+            $('#listContainer').append('<div id=' + array[i].id + '><form class="form-inline><div class="form-group"><h3>' + array[i].list + '</h3>' + hideButton + delButton + '</div> <div class="form-inline"><input type="text">' + createButton + '</div></form></div>');
         }
         else {
-            $('#'+ array[i].listid).append('<div class="form-check"><label class="form-check-label"><input class ="form-check-input" type="checkbox" value="">'+ array[i].task +'</label></div>');
+            if(array[i].complete == true){
+                $('#' + array[i].listid).append('<div class="form-check"><label id="' + array[i].id + '"class="form-check-label"><input checked class ="form-check-input" type="checkbox" value="">' + array[i].task + '</label></div>');
+
+            }else {
+            $('#' + array[i].listid).append('<div class="form-check"><label id="'+ array[i].id+'"class="form-check-label"><input class ="form-check-input" type="checkbox" value="">' + array[i].task + '</label></div>');
+            }
         }
     }
 }
-
-
